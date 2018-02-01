76ff2d545ac4e7c4ac57d10681bdfc13900c5114
Svitlana Kovalova <svitlana_kovalova1@epam.com>
2018-02-01 18:11:00 +0200

    [EPMRDKGRAD-11] Rename REST Contriller for run of docker, add comments

--- runner/src/main/java/com/epam/autograder/runner/controller/DockerController.java
+++ runner/src/main/java/com/epam/autograder/runner/controller/DockerController.java
@@ -1,9 +1,10 @@
-package com.epam.autograder.runner.resource;
+package com.epam.autograder.runner.controller;
 
 import com.epam.autograder.runner.entity.Submission;
 import com.epam.autograder.runner.result.Result;
 import com.epam.autograder.runner.service.DockerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
  * Create docker container.
  */
 @RestController
-public class DockerResource {
+public class DockerController {
+    /**
+     * @param dockerService contains 'runDocker' method
+     */
     @Autowired
+    @Qualifier("dockerServiceImpl")
     private DockerService dockerService;
-
+    /**
+     * @return  ResponseEntity<Result> in JSON format
+     * @param submission get from Core
+     */
     @PostMapping("/container/{containerName}")
     public ResponseEntity<Result> createContainer(@RequestBody Submission submission) {
         Result result = dockerService.runDocker(submission);
