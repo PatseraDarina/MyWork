63dbb65c47487a9c7c29b9cefc6d69c7a3fa290d
Valeriia Chub <valeriia_chub@epam.com>
2018-02-02 16:30:00 +0200

    [EPMRDKGRAD-7] - Minor fixes

--- core/src/test/java/com/epam/autograder/core/MockMvcBase.java
+++ core/src/test/java/com/epam/autograder/core/MockMvcBase.java
@@ -1,24 +1,5 @@
 package com.epam.autograder.core;
 
-import capital.scalable.restdocs.AutoDocumentation;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.restdocs.JUnitRestDocumentation;
-import org.springframework.restdocs.cli.CliDocumentation;
-import org.springframework.restdocs.http.HttpDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
 import static capital.scalable.restdocs.jackson.JacksonResultHandlers.prepareJackson;
 import static capital.scalable.restdocs.response.ResponseModifyingPreprocessors.limitJsonArrayLength;
 import static capital.scalable.restdocs.response.ResponseModifyingPreprocessors.replaceBinaryContent;
@@ -29,38 +10,52 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.cli.CliDocumentation;
+import org.springframework.restdocs.http.HttpDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import capital.scalable.restdocs.AutoDocumentation;
+
 /**
  * mockMvcBase test
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class MockMvcBase {
 
-    protected static final String CLASS_METHOD_NAME = "{class-name}/{method-name}";
-
-    @Autowired
-    private ApplicationContext context;
-
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    protected MockMvc mockMvc;
-
+    private static final String CLASS_METHOD_NAME = "{class-name}/{method-name}";
     @Rule
     public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+    protected MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private WebApplicationContext context;
 
     /**
      * setUp method
-     *
-     * @throws Exception check on exception
      */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         String http = "http";
         String localhost = "localhost";
         int port = 8080;
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup((WebApplicationContext) context)
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
                 .alwaysDo(prepareJackson(objectMapper))
                 .alwaysDo(commonDocumentation())
                 .apply(documentationConfiguration(restDocumentation)
--- core/src/test/java/com/epam/autograder/core/resource/SubmissionResourceTest.java
+++ core/src/test/java/com/epam/autograder/core/resource/SubmissionResourceTest.java
@@ -7,17 +7,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.nio.charset.Charset;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.epam.autograder.core.MockMvcBase;
 
@@ -25,30 +19,20 @@ import com.epam.autograder.core.MockMvcBase;
  * Test class for testing SubmissionResource functionality
  */
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 public class SubmissionResourceTest extends MockMvcBase {
 
-    private MockMvc mockMvc;
-    @Autowired
-    private WebApplicationContext webApplicationContext;
     @Autowired
     private SubmissionResource submissionResource;
+
     private String requestBody = "{\"submissionId\" : \"\", \"environmentId\" : \"gcdp_autograder_hello_world\", "
             + "\"inputSource\" : \"GIT\",  \"inputData\" : \"git@git.epam.com:.../...git\"}";
+
     private MediaType applicationJsonUtf8 = new MediaType(
             MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
     /**
-     * Before tests
-     */
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
-
-    /**
-     * Assest that submissionResource is not null
+     * ghghghg
      */
     @Test
     public void contextLoads() {
@@ -56,26 +40,28 @@ public class SubmissionResourceTest extends MockMvcBase {
     }
 
     /**
-     *
-     * @throws Exception
+     * gfghfjghfj,df
+     * @throws Exception fdffjdkjf
      */
+
     @Test
-    void testPostIsOk() throws Exception {
+    public void testPostIsOk() throws Exception {
         assertThat(mockMvc.perform(RestDocumentationRequestBuilders.post("/submission").content(requestBody)
                 .contentType(applicationJsonUtf8))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.submissionId", is(1)))
+                .andExpect(jsonPath("$.submissionId", is(2)))
                 .andExpect(jsonPath("$.environmentId", is("gcdp_autograder_hello_world")))
                 .andExpect(jsonPath("$.inputSource", is("GIT")))
                 .andExpect(jsonPath("$.inputData", is("git@git.epam.com:.../...git"))));
     }
 
     /**
+     * hgfghgghfgh
      *
-     * @throws Exception
+     * @throws Exception ghhkkfj
      */
     @Test
-    void testPost400() throws Exception {
+    public void testPost400() throws Exception {
         assertThat(mockMvc.perform(RestDocumentationRequestBuilders.post("/submission121").content(requestBody)
                 .contentType(applicationJsonUtf8))
                 .andExpect(status().is4xxClientError()));
