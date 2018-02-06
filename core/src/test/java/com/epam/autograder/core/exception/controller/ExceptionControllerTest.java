34ffa597ec3f1d6ff0e160c81b9412892e2f1e42
Eduard Khachirov <Eduard_Khachirov@epam.com>
2018-02-06 20:29:05 +0200

    [EPMRDKGRAD-55] - replace stub controller to real one

--- core/src/test/java/com/epam/autograder/core/exception/controller/ExceptionControllerTest.java
+++ core/src/test/java/com/epam/autograder/core/exception/controller/ExceptionControllerTest.java
@@ -2,22 +2,26 @@ package com.epam.autograder.core.exception.controller;
 
 import com.epam.autograder.core.CoreApplication;
 import com.epam.autograder.core.exception.BusinessException;
-import com.epam.autograder.core.exception.StubController;
+import com.epam.autograder.core.resource.SubmissionResource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.nio.charset.Charset;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,12 +38,19 @@ public class ExceptionControllerTest {
     private static final String JSON_RESPONSE_STATUS_CODE_FIELD = "$.statusCode";
     private static final String JSON_RESPONSE_STATUS_NAME_FIELD = "$.statusName";
     private static final String JSON_RESPONSE_DESCRIPTION_FIELD = "$.description";
+    private static final String REQUEST_URL = "/submission";
+    private static final String SUBMISSION_JSON = "{\"submissionId\" : \"\","
+            + " \"environmentId\" : \"gcdp_autograder_hello_world\", "
+            + "\"inputSource\" : \"GIT\","
+            + "  \"inputData\" : \"git@git.epam.com:.../...git\"}";
+
+    private static final MediaType JSON_MEDIA_TYPE = new MediaType(MediaType.APPLICATION_JSON, Charset.forName("utf8"));
 
     @Autowired
     private ExceptionController exceptionController;
 
     @Mock
-    private StubController stubController;
+    private SubmissionResource stubController;
 
     private MockMvc mockMvc;
 
@@ -52,6 +63,7 @@ public class ExceptionControllerTest {
                 .standaloneSetup(stubController)
                 .setControllerAdvice(exceptionController)
                 .build();
+
     }
 
     /**
@@ -61,17 +73,17 @@ public class ExceptionControllerTest {
      */
     @Test
     public void shouldReturnBadRequestStatusAndExceptionInformationWhenThrowingBusinessException() throws Exception {
-        String requestUrl = "/throwException";
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        String description = "BusinessException";
 
-        when(stubController.handleRequest()).thenThrow(new BusinessException(description));
+        HttpStatus expectedHttpStatus = HttpStatus.BAD_REQUEST;
+        String expectedDescription = "BusinessException";
 
-        assertThat(mockMvc.perform(get(requestUrl))
+        when(stubController.createSubmission(any())).thenThrow(new BusinessException(expectedDescription));
+
+        assertThat(mockMvc.perform(post(REQUEST_URL).contentType(JSON_MEDIA_TYPE).content(SUBMISSION_JSON))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath(JSON_RESPONSE_STATUS_CODE_FIELD, is(httpStatus.value())))
-                .andExpect(jsonPath(JSON_RESPONSE_STATUS_NAME_FIELD, is(httpStatus.name())))
-                .andExpect(jsonPath(JSON_RESPONSE_DESCRIPTION_FIELD, is(description))));
+                .andExpect(jsonPath(JSON_RESPONSE_STATUS_CODE_FIELD, is(expectedHttpStatus.value())))
+                .andExpect(jsonPath(JSON_RESPONSE_STATUS_NAME_FIELD, is(expectedHttpStatus.name())))
+                .andExpect(jsonPath(JSON_RESPONSE_DESCRIPTION_FIELD, is(expectedDescription))));
     }
 
     /**
@@ -81,17 +93,16 @@ public class ExceptionControllerTest {
      */
     @Test
     public void shouldReturnInternalServerErrorStatusAndExceptionInformationWhenThrowingAnyRuntimeException() throws Exception {
-        String requestUrl = "/throwException";
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        String description = "RuntimeException";
+        HttpStatus expectedHttpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        String expectedDescription = "RuntimeException";
 
-        when(stubController.handleRequest()).thenThrow(new RuntimeException(description));
+        when(stubController.createSubmission(any())).thenThrow(new RuntimeException(expectedDescription));
 
-        assertThat(mockMvc.perform(get(requestUrl))
+        assertThat(mockMvc.perform(post(REQUEST_URL).contentType(JSON_MEDIA_TYPE).content(SUBMISSION_JSON))
                 .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath(JSON_RESPONSE_STATUS_CODE_FIELD, is(httpStatus.value())))
-                .andExpect(jsonPath(JSON_RESPONSE_STATUS_NAME_FIELD, is(httpStatus.name())))
-                .andExpect(jsonPath(JSON_RESPONSE_DESCRIPTION_FIELD, is(description))));
+                .andExpect(jsonPath(JSON_RESPONSE_STATUS_CODE_FIELD, is(expectedHttpStatus.value())))
+                .andExpect(jsonPath(JSON_RESPONSE_STATUS_NAME_FIELD, is(expectedHttpStatus.name())))
+                .andExpect(jsonPath(JSON_RESPONSE_DESCRIPTION_FIELD, is(expectedDescription))));
     }
 
 }
